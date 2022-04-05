ce97f71ffaf77d9db1ad94cb42917e1165ae56aa
Gulmurod Yuldoshov <gystiu1995@gmail.com>
2022-04-05 13:22:56 +0500

    btn

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var toggleBtn =document.querySelector('.toggle')
+var elNav =document.querySelector('.nav')
+
+toggleBtn.addEventListener('click' , function(){
+    elNav.addEventListener.toggle('.nav-open')
+    toggleBtn.addEventListener.toggle ('.toggle-close')
+}) 
